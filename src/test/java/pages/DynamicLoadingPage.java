ad2a9e8973e84226ee7329009b1596cb3196ee44
Kateryna.Orlyk <Kateryna.Orlyk@avtor.ua>
2025-06-25 20:01:39 +0300

    feat: add DynamicLoadingPage extending BasePage

--- src/test/java/pages/DynamicLoadingPage.java
+++ src/test/java/pages/DynamicLoadingPage.java
@@ -1,4 +1,36 @@
 package pages;
 
-public class DynamicLoadingPage {
+import core.BasePage;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DynamicLoadingPage extends BasePage {
+
+    @FindBy(css = "#start button")
+    private WebElement startButton;
+
+    @FindBy(css = "#finish h4")
+    private WebElement resultText;
+
+    public DynamicLoadingPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Open dynamic page")
+    public void openPage() {
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+    }
+
+    @Step("Click Start Button")
+    public void clickStart() {
+        click(startButton);
+    }
+
+    @Step("Get loaded text result")
+    public String getLoadedText() {
+        return textOf(resultText);
+    }
+
 }
