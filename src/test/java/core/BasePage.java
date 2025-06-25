896be902b7b05140d64777adbe90299e72a396ae
Kateryna.Orlyk <Kateryna.Orlyk@avtor.ua>
2025-06-25 17:29:07 +0300

    feat: introduce BasePage with PageFactory, click/type/text methods

--- src/test/java/core/BasePage.java
+++ src/test/java/core/BasePage.java
@@ -1,4 +1,40 @@
 package core;
 
-public class BasePage {
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait   = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    @Step("Click on {element}")
+    protected void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+
+    @Step("Type '{text}' into {element}")
+    protected void type(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    @Step("Get text from {element}")
+    protected String textOf(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return element.getText().trim();
+    }
 }
